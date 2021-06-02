723c9b48b757cc6690151894d65274636d5bc10c
Clee681 <christopher.lee681@gmail.com>
2021-06-02 16:20:47 -0400

    web: show connect component when not connected on shop page

--- web/src/routes/shop/[id].svelte
+++ web/src/routes/shop/[id].svelte
@@ -14,6 +14,7 @@
   import type { MLBverseNS } from "$lib/types";
   import { ipfsUrl } from "$lib/utils";
   import { ethProvider, connected, currentAccount } from "$lib/stores/ethProvider";
+  import PleaseConnect from "$lib/components/PleaseConnect.svelte";
   import { ethers } from "ethers";
 
   export let data: MLBverseNS.ShopItemData;
@@ -67,56 +68,58 @@
   }
 
   async function redeem(ticketTokenId: ethers.BigNumber) {
-    console.log(ticketTokenId);
-    console.log(highlightTokenId);
     const r = await highlightsContract.redeem(ticketTokenId, highlightTokenId);
     console.log(r);
   }
 </script>
 
-<div class="px-32 flex flex-col lg:flex-row gap-4 mt-12">
-  <div class="flex flex-col flex-1 gap-4">
-    <div class="w-full border border-gray-300 rounded-md">
-      <img
-        class="w-full rounded-md"
-        src="https://lh3.googleusercontent.com/iJoQL40UzY-pBR7ijC7fXYYiKCqHnLo24_WLmwFsItCv3q5K3Mya0wMDy32fUvO9pHg0KBfrnpaU2Cf-pptQOzhO1dZJ-w89FyqP=w600"
-        alt="Field Vision"
-      />
-    </div>
-    {#if highlight}
-      <div>
-        <h2 class="text-3xl text-gray-700 font-semibold my-2">{highlight.name}</h2>
-        <h6 class="text-sm text-gray-500 font-semibold">
-          <span>Owned By:</span>
-          <span class="text-blue-400">{highlightOwner}</span>
-        </h6>
+{#if $connected}
+  <div class="px-32 flex flex-col lg:flex-row gap-4 mt-12">
+    <div class="flex flex-col flex-1 gap-4">
+      <div class="w-full border border-gray-300 rounded-md">
+        <img
+          class="w-full rounded-md"
+          src="https://lh3.googleusercontent.com/iJoQL40UzY-pBR7ijC7fXYYiKCqHnLo24_WLmwFsItCv3q5K3Mya0wMDy32fUvO9pHg0KBfrnpaU2Cf-pptQOzhO1dZJ-w89FyqP=w600"
+          alt="Field Vision"
+        />
       </div>
-    {/if}
-    <div class="w-full border border-gray-300 rounded-md">
-      <h2 class="p-4 font-semibold text-gray-700 border-b border-gray-300">Description</h2>
       {#if highlight}
-        <p class="p-4 text-sm text-gray-500">{highlight.description}</p>
+        <div>
+          <h2 class="text-3xl text-gray-700 font-semibold my-2">{highlight.name}</h2>
+          <h6 class="text-sm text-gray-500 font-semibold">
+            <span>Owned By:</span>
+            <span class="text-blue-400">{highlightOwner}</span>
+          </h6>
+        </div>
       {/if}
+      <div class="w-full border border-gray-300 rounded-md">
+        <h2 class="p-4 font-semibold text-gray-700 border-b border-gray-300">Description</h2>
+        {#if highlight}
+          <p class="p-4 text-sm text-gray-500">{highlight.description}</p>
+        {/if}
+      </div>
+    </div>
+    <div class="flex flex-col flex-1">
+      <div class="w-full border border-gray-300 rounded-md">
+        <h2 class="p-4 font-semibold text-gray-700 border-b border-gray-300">Redeem With Tickets</h2>
+        {#if tickets.length === 0}
+          <p class="p-4 text-sm text-gray-500">
+            Sorry, you have no redeemible tickets. You can purchase ticket stubs in our marketplace or get your own
+            ticket stub from attending a game.
+          </p>
+        {:else}
+          {#each tickets as t}
+            <div class="flex items-baseline justify-between px-6 py-4 text-sm">
+              <div class="text-gray-400">{`${t.section}-${t.row}-${t.seat}`}</div>
+              <button class="uppercase text-purple-500 focus:outline-none" on:click={() => redeem(t.tokenId)}
+                >Redeem</button
+              >
+            </div>
+          {/each}
+        {/if}
+      </div>
     </div>
   </div>
-  <div class="flex flex-col flex-1">
-    <div class="w-full border border-gray-300 rounded-md">
-      <h2 class="p-4 font-semibold text-gray-700 border-b border-gray-300">Redeem With Tickets</h2>
-      {#if tickets.length === 0}
-        <p class="p-4 text-sm text-gray-500">
-          Sorry, you have no redeemible tickets. You can purchase ticket stubs in our marketplace or get your own ticket
-          stub from attending a game.
-        </p>
-      {:else}
-        {#each tickets as t}
-          <div class="flex items-baseline justify-between px-6 py-4 text-sm">
-            <div class="text-gray-400">{`${t.section}-${t.row}-${t.seat}`}</div>
-            <button class="uppercase text-purple-500 focus:outline-none" on:click={() => redeem(t.tokenId)}
-              >Redeem</button
-            >
-          </div>
-        {/each}
-      {/if}
-    </div>
-  </div>
-</div>
+{:else}
+  <PleaseConnect />
+{/if}
